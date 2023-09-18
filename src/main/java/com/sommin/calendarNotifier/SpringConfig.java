747a07aedaeaadacab2a68a482bacebb3ba8ed61
smheo <heosm0402@gmail.com>
2023-09-18 15:18:25 +0900

    [Feature] Bean setting
    This commit added new bean setting for spring.

--- src/main/java/com/sommin/calendarNotifier/SpringConfig.java
+++ src/main/java/com/sommin/calendarNotifier/SpringConfig.java
@@ -1,4 +1,15 @@
 package com.sommin.calendarNotifier;
 
+import com.sommin.calendarNotifier.service.NotionService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
+
+    @Bean
+    public NotionService notionService() {
+        return new NotionService();
+    }
+
 }
