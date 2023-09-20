8f0af3931522029e9760c7357a90f2a482fe1773
smheo <heosm0402@gmail.com>
2023-09-20 16:16:40 +0900

    [Feature] New field `payload`
    This commit added new field `payload` in `DeˆfaultResponse`.

--- src/main/java/com/sommin/calendarNotifier/domain/DefaultResponse.java
+++ src/main/java/com/sommin/calendarNotifier/domain/DefaultResponse.java
@@ -6,13 +6,13 @@ public class DefaultResponse {
     private HttpStatus status;
     private String msg;
 
-    private String payload;
+    private Object payload;
 
-    public String getPayload() {
+    public Object getPayload() {
         return payload;
     }
 
-    public void setPayload(String payload) {
+    public void setPayload(Object payload) {
         this.payload = payload;
     }
 
