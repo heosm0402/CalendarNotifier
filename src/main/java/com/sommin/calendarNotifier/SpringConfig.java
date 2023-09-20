72c38c6010fb187c9cfd048361c31f4059c903d0
smheo <heosm0402@gmail.com>
2023-09-20 16:17:18 +0900

    [Feature] Spring configuration
    This commit set Bean for spring config.

--- src/main/java/com/sommin/calendarNotifier/SpringConfig.java
+++ src/main/java/com/sommin/calendarNotifier/SpringConfig.java
@@ -1,15 +1,25 @@
 package com.sommin.calendarNotifier;
 
+import com.sommin.calendarNotifier.repository.NotionObjectMemoryRepository;
+import com.sommin.calendarNotifier.repository.NotionRepository;
+import com.sommin.calendarNotifier.service.NotionClient;
 import com.sommin.calendarNotifier.service.NotionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-
+    @Bean
+    public NotionClient notionClient() {
+        return new NotionClient(notionRepository());
+    }
     @Bean
     public NotionService notionService() {
-        return new NotionService();
+        return new NotionService(notionClient());
+    }
+    @Bean
+    public NotionRepository notionRepository() {
+        return new NotionObjectMemoryRepository();
     }
 
 }
