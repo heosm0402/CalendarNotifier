a43c4f5181de2fa0bfd55374384117ae6e4a5fb4
smheo <heosm0402@gmail.com>
2023-09-20 16:19:31 +0900

    [Feature] Notion Service
    This commit implemented methods `listAllUserInfo` and skeleton of `extractCalendarData`.

--- src/main/java/com/sommin/calendarNotifier/service/NotionService.java
+++ src/main/java/com/sommin/calendarNotifier/service/NotionService.java
@@ -1,11 +1,35 @@
 package com.sommin.calendarNotifier.service;
 
+import com.google.gson.Gson;
+import com.sommin.calendarNotifier.domain.NotionMemberObject;
+import org.json.JSONArray;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class NotionService {
+    private final NotionClient notionClient;
+    private final Gson gson = new Gson();
 
-    private final String notionApiUrl = "";
+    public NotionService(NotionClient notionClient) {
+        this.notionClient = notionClient;
+    }
+
     public void extractCalendarData(LocalDate localDate) {
+        System.out.println("local date: " + localDate);
+        notionClient.extractDatabase("회의/출장/휴가");
 
     }
+
+    public ArrayList<NotionMemberObject> listAllUserInfo() {
+        JSONArray r = notionClient.listAllUsers();
+        ArrayList<NotionMemberObject> notionMemberObjectArrayList = new ArrayList<>();
+        for (Object memberObject : r) {
+            notionMemberObjectArrayList.add(gson.fromJson(memberObject.toString(), NotionMemberObject.class));
+        }
+
+        return notionMemberObjectArrayList;
+    }
+
+
 }
