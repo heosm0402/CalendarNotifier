b363f7a01bf01bca69b61ab2ed4b46185ee89f4c
smheo <heosm0402@gmail.com>
2023-10-04 17:19:19 +0900

    [Feature] Logic for extract calendar data
    This commit wrote main logic code for extract calendar database data.

--- src/main/java/com/sommin/calendarNotifier/service/NotionService.java
+++ src/main/java/com/sommin/calendarNotifier/service/NotionService.java
@@ -1,7 +1,8 @@
 package com.sommin.calendarNotifier.service;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.sommin.calendarNotifier.domain.NotionMemberObject;
+import com.sommin.calendarNotifier.domain.NotionPageObject;
 import org.json.JSONArray;
 
 import java.time.LocalDate;
@@ -15,10 +16,39 @@ public class NotionService {
         this.notionClient = notionClient;
     }
 
-    public void extractCalendarData(LocalDate localDate) {
+    public ArrayList<NotionPageObject> extractCalendarData(LocalDate localDate) {
         System.out.println("local date: " + localDate);
-        notionClient.extractDatabase("회의/출장/휴가");
+        String result = notionClient.extractDatabase("회의/출장/휴가");
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(result).getAsJsonObject();
+        JsonArray jsonArrayForEventNotionPage = jsonObject.get("results").getAsJsonArray();
+        ArrayList<NotionPageObject> calendarPageDataArrayList = new ArrayList<>();
+        int i = 0;
+        for (JsonElement eventNotionPage : jsonArrayForEventNotionPage ) {
+            JsonObject eventNotionPageJson = eventNotionPage.getAsJsonObject();
 
+            NotionPageObject notionPageObject = new NotionPageObject();
+            notionPageObject.setUrl(eventNotionPageJson.get("url").getAsString());
+            notionPageObject.setObjectId(eventNotionPageJson.get("id").getAsString());
+
+            JsonObject notionPageProperties = eventNotionPageJson.get("properties").getAsJsonObject();
+            notionPageObject.setPageTitle(notionPageProperties.get("이름").getAsJsonObject().get("title").getAsJsonArray().get(0).getAsJsonObject().get("plain_text").getAsString());
+            JsonArray peopleJsonArray = notionPageProperties.get("대상자").getAsJsonObject().get("people").getAsJsonArray();
+            ArrayList<NotionMemberObject> peopleArrayList = new ArrayList<>();
+            for (JsonElement peopleObject : peopleJsonArray) {
+                JsonObject peopleJson = peopleObject.getAsJsonObject();
+                peopleArrayList.add(gson.fromJson(peopleJson.toString(), NotionMemberObject.class));
+            }
+            notionPageObject.setPeople(peopleArrayList);
+//            notionPageObject.setPeople(notionPageProperties.get("대상자").getAsJsonObject().get("people").getAsJsonArray());
+            notionPageObject.setStartDate(notionPageProperties.get("날짜").getAsJsonObject().get("date").getAsJsonObject().get("start").getAsString());
+            Boolean end = notionPageProperties.get("날짜").getAsJsonObject().get("date").getAsJsonObject().get("end").isJsonNull();
+            if (!end) {
+                notionPageObject.setEndDate(notionPageProperties.get("날짜").getAsJsonObject().get("date").getAsJsonObject().get("end").getAsString());
+            }
+            calendarPageDataArrayList.add(notionPageObject);
+        }
+        return calendarPageDataArrayList;
     }
 
     public ArrayList<NotionMemberObject> listAllUserInfo() {
