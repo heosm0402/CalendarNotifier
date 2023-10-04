092621f30f093392d97079ac53b96db8dd237692
smheo <heosm0402@gmail.com>
2023-10-04 17:20:58 +0900

    [Feature] Notion Controller
    This commit set mapping and notion services with ResponseEntity.

--- src/main/java/com/sommin/calendarNotifier/controller/NotionController.java
+++ src/main/java/com/sommin/calendarNotifier/controller/NotionController.java
@@ -1,15 +1,21 @@
 package com.sommin.calendarNotifier.controller;
 
-import com.sommin.calendarNotifier.domain.DefaultResponse;
+import com.google.gson.JsonObject;
 import com.sommin.calendarNotifier.domain.NotionMemberObject;
+import com.sommin.calendarNotifier.domain.NotionPageObject;
+import com.sommin.calendarNotifier.http.ApiResponse;
 import com.sommin.calendarNotifier.service.NotionService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -26,31 +32,44 @@ public class NotionController {
     }
 
     @GetMapping("")
-    public DefaultResponse notionController() {
-        DefaultResponse defaultResponse = new DefaultResponse();
-        defaultResponse.setStatus(HttpStatus.OK);
-        defaultResponse.setMsg("notion controller root");
-        return defaultResponse;
+    public ResponseEntity<ApiResponse> notionController() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setStatus(HttpStatus.OK);
+        apiResponse.setMessage("api for notion");
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        return new ResponseEntity<>(apiResponse, headers, HttpStatus.OK);
     }
 
     @GetMapping("/extract/{datetime}")
-    public DefaultResponse extractNotionCalendarData(@PathVariable("datetime") String datetime) {
+    public ResponseEntity<ApiResponse> extractNotionCalendarData(@PathVariable("datetime") String datetime) {
         LocalDate localDate = LocalDate.parse(datetime, DateTimeFormatter.ISO_DATE);
-        DefaultResponse defaultResponse = new DefaultResponse();
-        defaultResponse.setStatus(HttpStatus.OK);
-        defaultResponse.setMsg("Extract of notion calendar data " + datetime +  " is requested");
-        notionService.extractCalendarData(localDate);
+//        JsonObject calendarData = notionService.extractCalendarData(localDate, "회의/출장/휴가");
+        ArrayList<NotionPageObject> calendarPageDataArrayList = notionService.extractCalendarData(localDate);
 
-        return defaultResponse;
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setStatus(HttpStatus.OK);
+        apiResponse.setMessage("Extract of notion calendar data " + datetime +  " is requested");
+        apiResponse.setData(calendarPageDataArrayList);
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        return new ResponseEntity<>(apiResponse, headers, HttpStatus.OK);
     }
 
     @GetMapping("/users")
-    public DefaultResponse getAllUsers() {
+    public ResponseEntity<ApiResponse> getAllUsers() {
         ArrayList<NotionMemberObject> result = notionService.listAllUserInfo();
-        DefaultResponse defaultResponse = new DefaultResponse();
-        defaultResponse.setStatus(HttpStatus.OK);
-        defaultResponse.setMsg("list all users");
-        defaultResponse.setPayload(result);
-        return defaultResponse;
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setStatus(HttpStatus.OK);
+        apiResponse.setMessage("list all users");
+        apiResponse.setData(result);
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        return new ResponseEntity<>(apiResponse, headers, HttpStatus.OK);
     }
 }
